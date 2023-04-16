f22fb29c0782bc413877b7cf6f16da4b9cd58577
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-04-16 16:15:27 +0700

    feat: Add a Users List Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import AddUser from "./component/Users/AddUser";
+import UsersList from "./component/Users/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
